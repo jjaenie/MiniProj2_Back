94d09a352b7b92bc0fcc10a352d0e182fed3a442
Seeyou2000 <valentine0517@naver.com>
2024-08-22 17:21:05 +0900

    Initial SpringBoot Project

---
+++ src/main/java/com/miniproj2_back/MiniProj2BackApplication.java
@@ -0,0 +1,13 @@
+package com.miniproj2_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProj2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProj2BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miniproj2_back/MiniProj2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproj2_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProj2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
